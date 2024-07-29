191a5988735b96de6655e29c2b0d427446443590
Sharvani ikkurti <Sharvani.ikkurti@godigit.com>
2024-07-29 08:06:37 +0530

    Adding new contact to AddressBook

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class AddressBookMain {
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook addressBook = new AddressBook();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter contact details:");
+
+        System.out.print("First Name: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Last Name: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Address: ");
+        String address = scanner.nextLine();
+
+        System.out.print("City: ");
+        String city = scanner.nextLine();
+
+        System.out.print("State: ");
+        String state = scanner.nextLine();
+
+        System.out.print("ZIP: ");
+        String zip = scanner.nextLine();
+
+        System.out.print("Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Email: ");
+        String email = scanner.nextLine();
+
+        Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+        addressBook.addContact(newContact);
+
+        System.out.println("Contact added successfully!");
+        System.out.println("Current contacts in address book:");
+        addressBook.displayContacts();
+    }
 }
